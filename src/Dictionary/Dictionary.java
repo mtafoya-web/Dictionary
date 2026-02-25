8e58c35aa41af646e51f1f8453e2af95f491f136
Mario <tafoya1897@gmail.com>
2026-02-25 01:12:47 -0800

    Created the dictionary form. Developed the front end of the desktop app.

---
+++ src/Dictionary/Dictionary.java
@@ -0,0 +1,57 @@
+package Dictionary;
+
+import javax.swing.*;
+
+public class Dictionary {
+
+    private JPanel app;
+    private JTextField searchWordTextField;
+    private JButton btn_find;
+    private JButton btn_add;
+    private JButton btn_delete;
+    private JLabel searchWord;
+    private JButton btn_exit;
+    private JPanel search_panel;
+    private JSplitPane splitMain;
+    private JPanel listPanel;
+    private JPanel detailsPanel;
+    private JLabel word;
+    private JTextField wordText;
+    private JLabel pron;
+    private JTextField pron_text_field;
+    private JButton btn_audio;
+    private JPanel pronPanel;
+    private JLabel example;
+    private JLabel def;
+    private JLabel syn;
+    private JScrollPane defScroll;
+    private JTextArea defTextArea;
+    private JScrollPane examScroll;
+    private JTextArea examTextArea;
+    private JScrollPane synScroll;
+    private JTextArea synTextArea;
+    private JPanel history;
+    private JPanel detailsBtn;
+    private JButton btn_edit;
+    private JButton btn_clear;
+    private JButton btn_save;
+    private JScrollPane scroll;
+    private JList lstWords;
+    private JPanel list_search;
+    private JPanel fullListPanel;
+    private JLabel searchWordList;
+    private JTextField textField1;
+    private JPanel historyPanel;
+    private JLabel hist;
+    private JButton btn_freq2;
+    private JButton btn_clearHistory;
+    private JButton btn_freq5;
+    private JButton btn_freq4;
+    private JButton btn_freq3;
+    private JButton btn_freq1;
+    private JLabel count;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
